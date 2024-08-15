fb4a248094bd8a516ca205df7b408a36a23b0bd4
Eli <elimane.sall@radiofrance.com>
2024-08-15 18:06:03 +0200

    main- random logo on the welcome page

--- welcomePage.js
+++ welcomePage.js
@@ -1,4 +1,77 @@
-const emptyPageLogo = `
+
+const neovim1 = `
+                                                                       
+    ██████   █████                   █████   █████  ███                  
+   ░░██████ ░░███                   ░░███   ░░███  ░░░                   
+    ░███░███ ░███   ██████   ██████  ░███    ░███  ████  █████████████   
+    ░███░░███░███  ███░░███ ███░░███ ░███    ░███ ░░███ ░░███░░███░░███  
+    ░███ ░░██████ ░███████ ░███ ░███ ░░███   ███   ░███  ░███ ░███ ░███  
+    ░███  ░░█████ ░███░░░  ░███ ░███  ░░░█████░    ░███  ░███ ░███ ░███  
+    █████  ░░█████░░██████ ░░██████     ░░███      █████ █████░███ █████ 
+   ░░░░░    ░░░░░  ░░░░░░   ░░░░░░       ░░░      ░░░░░ ░░░░░ ░░░ ░░░░░  
+                                                                        
+  `
+
+const neovim2 = `
+  ███▄    █ ▓█████  ▒█████   ██▒   █▓ ██▓ ███▄ ▄███▓ 
+  ██ ▀█   █ ▓█   ▀ ▒██▒  ██▒▓██░   █▒▓██▒▓██▒▀█▀ ██▒ 
+  ▓██  ▀█ ██▒▒███   ▒██░  ██▒ ▓██  █▒░▒██▒▓██    ▓██░ 
+  ▓██▒  ▐▌██▒▒▓█  ▄ ▒██   ██░  ▒██ █░░░██░▒██    ▒██  
+  ▒██░   ▓██░░▒████▒░ ████▓▒░   ▒▀█░  ░██░▒██▒   ░██▒ 
+  ░ ▒░   ▒ ▒ ░░ ▒░ ░░ ▒░▒░▒░    ░ ▐░  ░▓  ░ ▒░   ░  ░ 
+  ░ ░░   ░ ▒░ ░ ░  ░  ░ ▒ ▒░    ░ ░░   ▒ ░░  ░      ░ 
+    ░   ░ ░    ░   ░ ░ ░ ▒       ░░   ▒ ░░      ░    
+          ░    ░  ░    ░ ░        ░   ░         ░    
+                                 ░                   
+`;
+
+const neovim3 = `
+ ███▄▄▄▄      ▄████████  ▄██████▄   ▄█    █▄   ▄█    ▄▄▄▄███▄▄▄▄   
+ ███▀▀▀██▄   ███    ███ ███    ███ ███    ███ ███  ▄██▀▀▀███▀▀▀██▄ 
+ ███   ███   ███    █▀  ███    ███ ███    ███ ███▌ ███   ███   ███ 
+ ███   ███  ▄███▄▄▄     ███    ███ ███    ███ ███▌ ███   ███   ███ 
+ ███   ███ ▀▀███▀▀▀     ███    ███ ███    ███ ███▌ ███   ███   ███ 
+ ███   ███   ███    █▄  ███    ███ ███    ███ ███  ███   ███   ███ 
+ ███   ███   ███    ███ ███    ███ ███    ███ ███  ███   ███   ███ 
+  ▀█   █▀    ██████████  ▀██████▀   ▀██████▀  █▀    ▀█   ███   █▀  
+`;
+
+const neovim4 = `
+	                                                                       
+	                                                                     
+	       ████ ██████           █████      ██                     
+	      ███████████             █████                             
+	      █████████ ███████████████████ ███   ███████████   
+	     █████████  ███    █████████████ █████ ██████████████   
+	    █████████ ██████████ █████████ █████ █████ ████ █████   
+	  ███████████ ███    ███ █████████ █████ █████ ████ █████  
+	 ██████  █████████████████████ ████ █████ █████ ████ ██████ 
+	                                                                       
+
+`;
+
+const neovim5 = `
+	                                                                         
+	                               :                                         
+	 L.                     ,;    t#,                                        
+	 EW:        ,ft       f#i    ;##W.              t                        
+	 E##;       t#E     .E#t    :#L:WE              Ej            ..       : 
+	 E###t      t#E    i#W,    .KG  ,#D  t      .DD.E#,          ,W,     .Et 
+	 E#fE#f     t#E   L#D.     EE    ;#f EK:   ,WK. E#t         t##,    ,W#t 
+	 E#t D#G    t#E :K#Wfff;  f#.     t#iE#t  i#D   E#t        L###,   j###t 
+	 E#t  f#E.  t#E i##WLLLLt :#G     GK E#t j#f    E#t      .E#j##,  G#fE#t 
+	 E#t   t#K: t#E  .E#L      ;#L   LW. E#tL#i     E#t     ;WW; ##,:K#i E#t 
+	 E#t    ;#W,t#E    f#E:     t#f f#:  E#WW,      E#t    j#E.  ##f#W,  E#t 
+	 E#t     :K#D#E     ,WW;     f#D#;   E#K:       E#t  .D#L    ###K:   E#t 
+	 E#t      .E##E      .D#;     G#t    ED.        E#t :K#t     ##D.    E#t 
+	 ..         G#E        tt      t     t          E#t ...      #G      ..  
+	             fE                                 ,;.          j           
+	              ,                                                          
+`;
+
+
+
+const vim = `
                     .                    
     ##############..... ##############   
     ##############......##############   
@@ -18,11 +91,14 @@ const emptyPageLogo = `
       ######    .........                
                   .....                  
                     .`;
-
-
-const highlightedLogo = emptyPageLogo
-  .replace(/#/g, '<span style="color: limegreen;">#</span>')
-  .replace(/J/g, '<span style="color: limegreen;">#</span>')
-  ;
+                    
+const logoList = [ vim, neovim1, neovim2, neovim3, neovim4, neovim5 ];
+const randomLogo = logoList[Math.floor(Math.random() * logoList.length)];                
+const highlightedLogo = randomLogo === vim 
+  ? randomLogo.replace(/J/g,'#').replace(/[#J]/g, '<span style="color: limegreen;">$&</span>')
+  : randomLogo == neovim1 ? randomLogo.replace(/[█]/g, '<span style="color: limegreen;">$&</span>')
+  : randomLogo.replace(/./g, '<span style="color: limegreen;">$&</span>');
+  
 document.querySelector('.editor-group-watermark').innerHTML = `<pre>${highlightedLogo}</pre>`;
 document.querySelector('.watermark-box').innerHTML = `<pre>${highlightedLogo}</pre>`;
+document.querySelector('.shortcuts').innerHTML = `<pre>${highlightedLogo}</pre>`;
